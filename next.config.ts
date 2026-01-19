48b54153198764b34a3b95e685b3a2ad7cc3dc31
andrexxrojas <jayandrerojas@gmail.com>
2026-01-19 16:30:21 +0800

    chore: add icons package and optimize nextjs for icons modules

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  devIndicators: false,
+  experimental: {
+    optimizePackageImports: ["@phosphor-icons/react"]
+  }
 };
 
 export default nextConfig;
