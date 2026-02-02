1532723e4670d7104d7b4363bd6c52a528a1b3b9
andrexxrojas <jayandrerojas@gmail.com>
2026-02-03 00:31:12 +0800

    Add drag context provider in note editor

--- src/app/components/NoteEditor/NoteEditor.tsx
+++ src/app/components/NoteEditor/NoteEditor.tsx
@@ -4,6 +4,7 @@ import styles from "./NoteEditor.module.css";
 import { useBlocks } from "@/app/components/NoteEditor/hooks/useBlocks";
 import VirtualizedBlocks from "@/app/components/NoteEditor/blocks/VirtualizedBlocks";
 import { BlockActionsProvider } from "@/app/context/BlockActionsContext";
+import { DragProvider } from "@/app/context/DragContext";
 import BlockFactory from "@/app/components/NoteEditor/blocks/BlockFactory";
 
 export default function NoteEditor({ note, scrollContainerRef }: { note: Note, scrollContainerRef: React.RefObject<HTMLDivElement | null> }) {
@@ -19,13 +20,15 @@ export default function NoteEditor({ note, scrollContainerRef }: { note: Note, s
 			}}
 		>
 			<div className={styles["note-editor"]}>
-				{blocks.map(block => (
-					<BlockFactory
-						key={block.id}
-						block={block}
-						autoFocus={block.id === focusedBlockId}
-					/>
-				))}
+				<DragProvider>
+					{blocks.map(block => (
+						<BlockFactory
+							key={block.id}
+							block={block}
+							autoFocus={block.id === focusedBlockId}
+						/>
+					))}
+				</DragProvider>
 			</div>
 		</BlockActionsProvider>
 	);
