0509ccbb78a6be4f441de1216b34f1efc8ff7ac5
andrexxrojas <jayandrerojas@gmail.com>
2026-02-03 00:27:47 +0800

    Turn off virtualization
    New features to block causes new bugs to occur in virtualization.
    Turning off virtualization to help speed up development process.
    Virtualization will be turned back on and refactored once it is finally needed.

--- src/app/components/NoteEditor/NoteEditor.tsx
+++ src/app/components/NoteEditor/NoteEditor.tsx
@@ -4,6 +4,7 @@ import styles from "./NoteEditor.module.css";
 import { useBlocks } from "@/app/components/NoteEditor/hooks/useBlocks";
 import VirtualizedBlocks from "@/app/components/NoteEditor/blocks/VirtualizedBlocks";
 import { BlockActionsProvider } from "@/app/context/BlockActionsContext";
+import BlockFactory from "@/app/components/NoteEditor/blocks/BlockFactory";
 
 export default function NoteEditor({ note, scrollContainerRef }: { note: Note, scrollContainerRef: React.RefObject<HTMLDivElement | null> }) {
 	const { blocks, addBlockBelow, focusedBlockId, updateBlockContent, setFocusedBlockId } = useBlocks(note.id);
@@ -18,11 +19,13 @@ export default function NoteEditor({ note, scrollContainerRef }: { note: Note, s
 			}}
 		>
 			<div className={styles["note-editor"]}>
-				<VirtualizedBlocks
-					blocks={blocks}
-					scrollContainerRef={scrollContainerRef}
-					focusedBlockId={focusedBlockId}
-				/>
+				{blocks.map(block => (
+					<BlockFactory
+						key={block.id}
+						block={block}
+						autoFocus={block.id === focusedBlockId}
+					/>
+				))}
 			</div>
 		</BlockActionsProvider>
 	);
